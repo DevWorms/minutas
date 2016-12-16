2ff716d554e79f1d43154a6ca0925ca9f8f2c48c
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-12-16 02:54:23 -0600

    buscador

--- ios/Minutas/GraficasViewController.swift
+++ ios/Minutas/GraficasViewController.swift
@@ -9,12 +9,51 @@
 import Foundation
 
 import Charts
+
+
 class GraficasViewController: UIViewController{
     
+    
+    @IBOutlet weak var barView: BarChartView!
+    
+    var numeros = [Double]()
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        numeros.append(1)
+        numeros.append(2)
+        numeros.append(3)
+        
+        updateChartWithData()
         
     }
+    
+    func updateChartWithData() {
+        
+        
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        
+        //for i in 0..<numeros.count {
+        var dataEntry = BarChartDataEntry(value: 5 , xIndex:0 )
+        dataEntries.append(dataEntry)
+        
+        dataEntry = BarChartDataEntry(value: 2 , xIndex:1 )
+        dataEntries.append(dataEntry)
+        
+        dataEntry = BarChartDataEntry(value: 3 , xIndex:2 )
+        dataEntries.append(dataEntry)
+        //}
+        
+        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Prueba")
+        let chartData = BarChartData(xVals: ["hola", "mundo", "feliz"], dataSet: chartDataSet)
+        barView.data = chartData
+        
+        
+        barView.invalidateIntrinsicContentSize()
+    }
+    
+    
     
 }
