5fefe78efb94a6783798033521d8913e92526891
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-12-16 00:30:13 -0600

    gráficas
    es importante revisar la instalacion para poder ejecutar este commit
    https://github.com/danielgindi/Charts/tree/v2.3.0

---
+++ ios/Minutas/GraficasViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GraficasViewController.swift
+//  Minutas
+//
+//  Created by sergio ivan lopez monzon on 16/12/16.
+//  Copyright © 2016 Uriel Mestas Estrada. All rights reserved.
+//
+
+import Foundation
+
+import Charts
+class GraficasViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+    
+}
