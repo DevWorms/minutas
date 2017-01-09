0b227dd95f393f18e9717bb647792287836fc2af
sergio ivan lopez monzon <sergioivan154@gmail.com>
2017-01-08 19:32:02 -0600

    ventana chat

---
+++ ios/Minutas/ConversacionCell.swift
@@ -0,0 +1,29 @@
+
+//
+//  ConversacionCell.swift
+//  Minutas
+//
+//  Created by sergio ivan lopez monzon on 08/01/17.
+//  Copyright © 2017 Uriel Mestas Estrada. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConversacionCell: GenericCell {
+    
+    @IBOutlet weak var tituloChat: UILabel!
+    @IBOutlet weak var usuarios: UILabel!
+    
+    @IBOutlet weak var imagenUsuario: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        //  categoriaPicker.setValue(UIColor.whiteColor(), forKeyPath: "textColor")
+    }
+    
+    
+    
+}
+
---
+++ ios/Minutas/ConversacionesTableViewController.swift
@@ -0,0 +1,157 @@
+//
+//  ConversacionesTableViewController.swift
+//  Minutas
+//
+//  Created by sergio ivan lopez monzon on 08/01/17.
+//  Copyright © 2017 Uriel Mestas Estrada. All rights reserved.
+//
+
+//
+//  ReunionesTableViewController.swift
+//  Minutas
+//
+//  Created by sergio ivan lopez monzon on 04/12/16.
+//  Copyright © 2016 Uriel Mestas Estrada. All rights reserved.
+//
+
+import UIKit
+
+class ConversacionesTableViewController: UITableViewController{
+    
+    //Esta variable viene desde menu principal y hace referencia a los menus que deben de comprarse
+    
+    var conversaciones = [[String : AnyObject]]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        
+        loadConversaciones()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // Make the background color show through
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clearColor()
+        return headerView
+    }
+    
+    
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! ConversacionCell
+        
+        let json = conversaciones[indexPath.item]
+        
+        cell.tituloChat.text = json[WebServiceResponseKey.tituloChat] as? String
+        let miembros = json[WebServiceResponseKey.miembros] as! [[String : AnyObject]]
+        
+        for jsonMiembros in miembros{
+            
+            if !((cell.tituloChat.text?.isEmpty)!){
+                cell.usuarios.text = ", " + cell.tituloChat.text!
+            }
+            
+            cell.usuarios.text = jsonMiembros[WebServiceResponseKey.apodo] as? String
+            
+        }
+        
+        return cell
+        
+    }
+    
+    func newConversacionControllerDidCancel() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    func newConversacionControllerDidFinish() {
+        dismissViewControllerAnimated(true, completion: nil)
+        loadConversaciones()
+    }
+   
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.conversaciones.count
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let json = conversaciones[indexPath.item]
+        
+        NSUserDefaults.standardUserDefaults().setInteger(json[WebServiceResponseKey.conversacionId] as! Int, forKey: WebServiceResponseKey.conversacionId)
+        
+        
+        
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        
+    }
+    
+    
+    func loadConversaciones() {
+        let apiKey = NSUserDefaults.standardUserDefaults().valueForKey(WebServiceResponseKey.apiKey)!
+        let userId = NSUserDefaults.standardUserDefaults().integerForKey(WebServiceResponseKey.userId)
+        
+        print(apiKey, userId)
+        let str = "\(WebServiceEndpoint.baseUrl)\(WebServiceEndpoint.conversaciones)\(userId)/\(apiKey)/"
+        print(str)
+        let url = NSURL(string: str)!
+        NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: parseJson).resume()
+    }
+    
+    func parseJson(data: NSData?, urlResponse: NSURLResponse?, error: NSError?) {
+        if error != nil {
+            print(error!)
+        } else if urlResponse != nil {
+            if (urlResponse as! NSHTTPURLResponse).statusCode == HttpStatusCode.OK {
+                if let json = try? NSJSONSerialization.JSONObjectWithData(data!, options: []) {
+                    print(json)
+                    dispatch_async(dispatch_get_main_queue()) {
+                        if self.conversaciones.count > 0 {
+                            self.conversaciones.removeAll()
+                        }
+                        
+                        self.conversaciones.appendContentsOf(json[WebServiceResponseKey.conversaciones] as! [[String : AnyObject]])
+                        self.tableView?.reloadData()
+                    }
+                } else {
+                    print("HTTP Status Code: 200")
+                    print("El JSON de respuesta es inválido.")
+                }
+            } else {
+                dispatch_async(dispatch_get_main_queue()) {
+                    if let json = try? NSJSONSerialization.JSONObjectWithData(data!, options: []) {
+                        let vc_alert = UIAlertController(title: nil, message: json[WebServiceResponseKey.message] as? String, preferredStyle: .Alert)
+                        vc_alert.addAction(UIAlertAction(title: "OK", style: .Cancel , handler: nil))
+                        self.presentViewController(vc_alert, animated: true, completion: nil)
+                    } else {
+                        print("HTTP Status Code: 400 o 500")
+                        print("El JSON de respuesta es inválido.")
+                    }
+                }
+            }
+        }
+    }
+    
+    // para cuadrar las imagenes
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return 80
+    }
+    
+    
+    
+}
